e563ee15d938670ebcb8c9561c5a42738232a487
AntonioE313 <expositoa131@gmail.com>
2021-03-20 15:35:42 +0000

    Initial implementation of premature beats plus refactorish
    New feature classifies premature beats and in a secondary run of the coherent averaging algorithm the app excludes premature beats from contributing to the saecg. This new saecg is plotted

--- Project/config.py
+++ Project/config.py
@@ -1,4 +1,9 @@
 # Config file for easily changing app behaviour
 data_filepath = 'Data/e0103.csv'
 fs = 250
-signal_duration = 240 # Signal duration in seconds
+signal_duration = 60  # Signal duration in seconds
+
+p_window_center = 0.15  # Initial P window centre as in seconds back from peak of QRS
+saecg_window_size = int(0.2 * fs)  # Watch for rounding error here converting to int
+
+premature_signal_coefficient = 0.99  # Threshold for RR interval i in terms of RR interval (i-1) for premature state=1
